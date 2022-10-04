3d97633540a2868b788f515fcee1a69948bf4258
Acfboy <acfboyu@outlook.com>
2022-10-05 07:37:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name        不是谭炜谭中文 AT 大师
+// @name        AtCoder 中文助手
 // @namespace   Violentmonkey Scripts
 // @match       https://atcoder.jp/contests/*
 // @grant       none
-// @version     1.0
+// @license MIT
+// @version     1.4.1
 // @author      -
-// @description 2022/10/4 08:28:02
+// @description 在英文题面前显示 AtCoder 中文题面。
 // ==/UserScript==
 
 function addScript(url){
@@ -13,9 +14,6 @@ function addScript(url){
 	script.setAttribute('type','text/javascript');
 	script.setAttribute('src',url);
 	document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-function add2(url){
 }
 
 function createXmlHttp() {
@@ -37,13 +35,18 @@ function writeSource() {
     if (xmlHttp.readyState == 4) {
         var s = xmlHttp.responseText;
         s = s.split('<textarea>')
+        s[1] = s[1].split('数据范围')[0]
         var t = document.getElementsByClassName('lang-en')
-        var res = marked(s[1])
-        for (var i = 1; i <= 100; i++) {
-            if (i % 2 == 1) res = res.replace('$', '\\(')
-            else res = res.replace('$', '\\)')
-        }
-        t[0].innerHTML = res + t[0].innerHTML
+        var res = s[1];//marked(s[1])
+        var flag = 1
+        if (res[0] == '$') flag = 0
+        var a = res.split('$');
+        var fin;
+        for (var i = 0; i < a.length; i++)
+            if (i % 2 == flag) fin += katex.renderToString(a[i])
+            else fin += a[i]
+        fin = '<div class=\'chn\'>' + fin + '</div>'
+        t[0].innerHTML = marked(fin) + t[0].innerHTML
         // alert(s[1]);
     }
 }
@@ -58,9 +61,15 @@ addScript("https://cdn.bootcdn.net/ajax/libs/marked/2.0.3/marked.js")
 
 var script = document.createElement('script');
 script.setAttribute('type','text/javascript');
-script.setAttribute('src','https://cdn.bootcss.com/mathjax/3.0.5/es5/tex-mml-chtml.js');
-script.setAttribute('async','');
-script.setAttribute('id','MathJax-script');
+script.innerText = 'function disap() { var bo = document.getElementById(\'mybo\'); var c = document.getElementsByClassName(\'chn\')[0]; if (bo.innerHTML == \'隐藏中文题面\') {  c.style = \'display:none\'; bo.innerHTML = \'显示中文题面\' } else { c.style=\'\'; bo.innerHTML = \'隐藏中文题面\'  } }'
 document.getElementsByTagName('head')[0].appendChild(script);
+
+var pos = document.getElementsByClassName('h2')[0]
+var bo = document.createElement('a');
+bo.setAttribute('class', 'btn btn-default btn-sm')
+bo.setAttribute('id', 'mybo')
+bo.setAttribute('onclick', 'disap()')
+bo.innerHTML = '隐藏中文题面'
+pos.appendChild(bo)
 
 getSource(pr)
